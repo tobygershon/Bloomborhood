5504580fa3b9627b5e3bedfea8e2ff049dec2732
Toby Gershon <tobygershon@gmail.com>
2024-06-10 13:30:30 -0400

    css changes

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -24,18 +24,23 @@ export default function PostCard(props) {
                     <div className="columns is-gapless">
                         <div className="column ">
                             <div className="box box-1 is-flex is-flex-direction-column is-align-items-center is-justify-content-center has-text-weight-semibold">
-                                <p className="is-flex is-justify-content-center is-size-7 is-size-6-desktop is-flex is-align-items-center has-text-grey">Type of Plant: <span className="ml-1 mb-1 is-size-5 is-size-4-desktop has-text-dark">{props.post.plantName}</span></p>
-                                <p className="is-flex is-justify-content-center is-size-7 is-size-6-desktop has-text-grey">Description: <span className="ml-1 has-text-weight-semibold has-text-dark">{props.post.description}</span></p>
+                                <p className="is-flex is-justify-content-center is-size-7 is-size-6-desktop is-flex is-align-items-center has-text-grey"><i className="fa-solid fa-seedling is-size-5"></i><span className="ml-1 mb-1 is-size-5 is-size-4-desktop has-text-dark">{props.post.plantName}</span></p>
+                                
                             </div>
                         </div>
                         <div className="column">
                             <div className="box box-2 is-flex is-flex-direction-column is-align-items-center is-justify-content-center has-text-dark is-size-6 is-size-5-desktop has-text-weight-semibold">
-                                <span className="is-flex is-justify-content-center ">{props.post.wasRequested ? "Has been requested " + props.post.numberOfRequests + " time" + (props.post.numberOfRequests === 1 ? "" : "s") : "Be the first to request!"}</span>
-                                <span className="is-flex is-justify-content-center ">{props.post.wasRequested ? "Was first requested at " + props.post.firstRequestTime : ""}</span>
+                            <p className="is-flex is-justify-content-center is-size-7 is-size-6-desktop has-text-grey"><span className="ml-1 has-text-weight-semibold has-text-dark">{props.post.description}</span></p>
                             </div>
                         </div>
                         <div className="column">
                             <div className="box box-3 is-flex is-flex-direction-column is-align-items-center is-justify-content-center has-text-dark is-size-7 is-size-6-desktop ">
+                                
+                                    { !addressRequested && <div><span className="is-flex is-justify-content-center ">{props.post.wasRequested ? "Has been requested " + props.post.numberOfRequests + " time" + (props.post.numberOfRequests === 1 ? "" : "s") : "Be the first to request!"}</span>
+                                <span className="is-flex is-justify-content-center ">{props.post.wasRequested ? "Was first requested at " + props.post.firstRequestTime : ""}</span>
+                                </div>}
+                                
+                                
                                 <div>{!addressRequested && <button className="button" onClick={addressRequest}>Request Address</button>}</div>
                                 <div>{addressRequested && "Remember, the policy is first come first serve.  Right now your plants are still listed as available, but may not be by the time you pick up"}</div>
                             </div>
