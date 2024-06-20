e1b220d0c8638b9ac45b2927e8bdeffa527ac775
Toby Gershon <tobygershon@gmail.com>
2024-06-20 11:29:56 -0400

    try again

--- src/services/plantSearchWebAPIService.jsx
+++ src/services/plantSearchWebAPIService.jsx
@@ -1,5 +1,15 @@
 import axios from 'axios';
-import { getEnvVar } from './netlifyEnvVar';
+// import { getEnvVar } from './netlifyEnvVar';
+
+const bar = Netlify.env.get("PLANT_API_KEY")
+
+function getPlantAPI() {
+
+   console.log(new Response(bar));
+   
+}
+
+// export const config = { path: "/*" };
 
 const plantAPI = axios.create({
     baseURL: 'https://perenual.com/api'
@@ -7,7 +17,8 @@ const plantAPI = axios.create({
 
 
 export async function getAllPlants() {
-    const value =  await getEnvVar();
+    const value =  await getPlantAPI();
+    console.log(value);
     const response = await plantAPI.get(`species-list?key=${value}`);
     return response.data.data;
 }
